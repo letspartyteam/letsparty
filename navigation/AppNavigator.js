9d87d625ca364073b43abe0ec7e4d84fb02fe8e3
mayadamahmoud <mayada.elsaidd@gmail.com>
2019-11-09 17:10:22 +0300

    "Initial commit"

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+import MainTabNavigator from './MainTabNavigator';
+import LoginScreen from '../screens/LoginScreen'
+import AuthLoadingScreen from '../screens/AuthLoadingScreen'
+
+const AuthStack = createStackNavigator({ Login: LoginScreen });
+const switchNavigator = createSwitchNavigator(
+	{
+		Main: MainTabNavigator,
+		Auth: AuthStack, 
+		AuthLoading: AuthLoadingScreen,
+	},
+	{
+		initialRouteName: 'AuthLoading'
+	}
+);
+export default createAppContainer(switchNavigator)
---
+++ screens/LoginScreen.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import { 
+  View, 
+  Image,
+  Text, 
+  StyleSheet, 
+  Platform,
+  TextInput,
+  Alert,
+ } from 'react-native';
+import firebase from 'firebase';
+import { Button } from 'react-native-elements';
+import * as FirebaseAPI from '../modules/firebaseAPI';
+
+
+export default class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  static navigationOptions = {
+    //title: 'Login',
+    headerStyle: {
+        backgroundColor: '#F85F64',
+      },
+  };
+
+  state = {
+    email: "",
+    password: ""
+  };
+
+  componentDidMount() {
+    this.watchAuthState(this.props.navigation)
+  }
+
+  watchAuthState(navigation) {
+    firebase.auth().onAuthStateChanged(function(user) {
+      console.log('onAuthStatheChanged: ', user)      
+      if (user) {
+        navigation.navigate('Main');
+      }
+    });
+  }
+
+  createUser() {
+    FirebaseAPI.createUser(this.state.email, this.state.password);
+    alert('Invalid username or password');
+    /*try{
+    FirebaseAPI.createUser(this.state.email, this.state.password)
+    }catch(err){
+      alert('Invalid username or password'); }*/
+    }
+
+  signIn() {
+    FirebaseAPI.signInUser(this.state.email, this.state.password)
+    /*try{
+      FirebaseAPI.signInUser(this.state.email, this.state.password)
+      }catch(err){
+        alert('Invalid username or password'); }*/
+      }
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+              <Image
+              source={require('../assets/images/white-logo.png')}
+              style={{width:150, height:150, marginLeft:'35%'}}
+              />
+        <View style={styles.textContainer}>
+          <Text style={styles.logoText}>LetsParty</Text>
+          <TextInput 
+            style={styles.loginFormTextInput}
+            onChangeText={(text) => this.setState({email: text})}
+            placeholder="Email"
+            value={this.state.email}
+          />
+          <TextInput 
+            style={styles.loginFormTextInput}
+            onChangeText={(text) => this.setState({password: text})}
+            placeholder="Password"
+            secureTextEntry
+            value={this.state.password}
+          />
+        
+        <Button 
+            buttonStyle={styles.loginButton}
+            onPress={() => this.signIn()}
+            title="Sign in"
+        />
+
+        <Button 
+            buttonStyle={styles.loginButton}
+            onPress={() => this.createUser()}
+            title="Sign up
+            "
+        />
+
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F85F64',
+  },
+  textContainer: {
+    flex: 1,
+  },
+ 
+  loginFormTextInput: {
+    height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#eaeaea',
+    backgroundColor: '#fafafa',
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 5,
+    marginBottom: 5, 
+  },
+  
+  loginButton: {
+    backgroundColor: '#decbca',
+    borderRadius: 15,
+    height: 45,
+    width:'50%',
+    marginTop: 10,
+    marginLeft: '25%'
+  },
+
+  logoText: {
+    fontSize: 40,
+    fontWeight: "800",
+    marginTop: 30,
+    marginBottom: 30,
+    textAlign: 'center',
+    color:'#fff'
+  },
+});
